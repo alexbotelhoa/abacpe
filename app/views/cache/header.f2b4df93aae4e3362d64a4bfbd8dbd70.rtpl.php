04e948590950b4910dfd3b498fbdecb5ce675f52
Alex Botelho <alexbotelho1@hotmail.com>
2020-01-30 19:08:51 -0300

    Ajuste de Cabeçalho

--- app/views/cache/header.f2b4df93aae4e3362d64a4bfbd8dbd70.rtpl.php
+++ app/views/cache/header.f2b4df93aae4e3362d64a4bfbd8dbd70.rtpl.php
@@ -66,15 +66,9 @@
 	<div class="site-branding-area">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-6">
+				<div class="col-xs-6">
 					<div class="logo">
 						<h1><a href="#"><img src="/res/site/img/logo.png"></a></h1>
-					</div>
-				</div>
-
-				<div class="col-sm-6">
-					<div class="shopping-item">
-						<a href="#">Carrinho - <span class="cart-amunt">R$100</span> <i class="fa fa-shopping-cart"></i> <span class="product-count">5</span></a>
 					</div>
 				</div>
 			</div>
