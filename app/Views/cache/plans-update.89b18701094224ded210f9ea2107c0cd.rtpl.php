ee2be083becbd2db14ed51ffad2861246911c8ef
Alex Botelho <alexbotelho1@hotmail.com>
2020-06-27 00:59:16 -0300

    .

---
+++ app/Views/cache/plans-update.89b18701094224ded210f9ea2107c0cd.rtpl.php
@@ -0,0 +1,54 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="maincontent-area">
+    <div class="zigzag-bottom"></div>
+    <section class="content">
+        <div class="row">
+            <section class="container">
+                <ol class="breadcrumb">
+                    <li><a href="/abasce2"><i class="fa fa-tachometer"></i> Home</a></li>
+                    <li><a href="/abasce2/plans">Planos</a></li>
+                    <li class="active"><a href="/abasce2/plans/<?php echo htmlspecialchars( $plan["idplan"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/update">Editar</a></li>
+                </ol>
+            </section>
+            <section class="container">
+                <h2>Editar Plano</h2>
+            </section>
+            <section class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="box box-primary">
+                            <form role="form" action="/abasce2/plans/<?php echo htmlspecialchars( $plan["idplan"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/update" method="post" enctype="multipart/form-data">
+                                <div class="box-body">
+                                    <div class="form-group">
+                                        <label for="desplan">Nome Plano</label>
+                                        <input type="text" class="form-control" id="desplan" name="desplan"
+                                               placeholder="Digite o nome do produto" value="<?php echo htmlspecialchars( $plan["desplan"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="vlplan">Valor Plano</label>
+                                        <input type="number" class="form-control" id="vlplan" name="vlplan" step="0.01"
+                                               placeholder="0.00" value="<?php echo htmlspecialchars( $plan["vlplan"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+                                    </div>
+                                </div>
+                                <div class="box-footer">
+                                    <div class="col-md-8 col-xs-6">
+                                        <button type="submit" class="btn btn-success">Salvar</button>
+                                    </div>
+                                    <?php if( $error != '' ){ ?>
+
+                                    <div class="col-md-4 col-xs-6">
+                                        <div class="box-header bg-red">
+                                            <?php echo htmlspecialchars( $error, ENT_COMPAT, 'UTF-8', FALSE ); ?>
+
+                                        </div>
+                                    </div>
+                                    <?php } ?>
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
+</div>
---
+++ app/Views/cache/plans.89b18701094224ded210f9ea2107c0cd.rtpl.php
@@ -0,0 +1,71 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="maincontent-area">
+    <div class="zigzag-bottom"></div>
+    <section class="content">
+        <div class="row">
+            <section class="container">
+                <ol class="breadcrumb">
+                    <li><a href="/abasce2"><i class="fa fa-tachometer"></i> Home</a></li>
+                    <li class="active"><a href="/abasce2/plans">Planos</a></li>
+                </ol>
+            </section>
+            <section class="container">
+                <h2>Planos</h2>
+            </section>
+            <section class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="box box-primary">
+                            <div class="box-header">
+                                <div class="col-md-8 col-xs-6">
+                                    <a href="/abasce2/plans/create" class="btn btn-success">Cadastrar</a>
+                                </div>
+                                <?php if( $success != '' ){ ?>
+                                <div class="col-md-4 col-xs-6">
+                                    <div class="box-header bg-green">
+                                        <?php echo htmlspecialchars( $success, ENT_COMPAT, 'UTF-8', FALSE ); ?>
+                                    </div>
+                                </div>
+                                <?php } ?>
+                            </div>
+                            <div class="box-body no-padding">
+                                <table class="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th style="width: 10px"><a href="/abasce2/order/plans/ordid">#</a></th>
+                                        <th><a href="/abasce2/order/plans/ordplan">Nome Plano</a></th>
+                                        <th><a href="/abasce2/order/plans/ordvlp">Valor Plano</a></th>
+                                        <th style="width: 140px">&nbsp;</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php $counter1=-1;  if( isset($plan) && ( is_array($plan) || $plan instanceof Traversable ) && sizeof($plan) ) foreach( $plan as $key1 => $value1 ){ $counter1++; ?>
+                                    <tr>
+                                        <td><?php echo htmlspecialchars( $value1["idplan"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>
+                                        <td><?php echo htmlspecialchars( $value1["desplan"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>
+                                        <td><?php echo formatPrice($value1["vlplan"]); ?></td>
+                                        <td>
+                                            <a href="/abasce2/plans/<?php echo htmlspecialchars( $value1["idplan"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/update" class="btn btn-primary btn-xs"><i class="fa fa-edit"></i> Editar</a>
+                                            <a href="/abasce2/plans/<?php echo htmlspecialchars( $value1["idplan"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/delete" onclick="return confirm('Deseja realmente excluir este registro?')"
+                                               class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> Excluir</a>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="product-pagination text-center">
+                            <nav>
+                                <ul class="pagination">
+                                    <?php $counter1=-1;  if( isset($pages) && ( is_array($pages) || $pages instanceof Traversable ) && sizeof($pages) ) foreach( $pages as $key1 => $value1 ){ $counter1++; ?>
+                                    <li><a href="<?php echo htmlspecialchars( $value1["link"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["page"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></li>
+                                    <?php } ?>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
+</div>
